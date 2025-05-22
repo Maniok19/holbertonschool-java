5878cadf32132527ed8ff87be444f4495a54314b
Maniok19 <mano19029@hotmail.fr>
2025-05-22 13:00:41 +0200

    Fix syntax error in PrintStringVariable.java by adding missing semicolon

--- 0x00-java-hello_world/PrintStringVariable.java
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -1,6 +1,6 @@
 public class PrintStringVariable {
     public static void main(String[] args) {
         String text = "Holberton School";
-        System.out.printf("%s\n%s\n%s\n", text, text, text.substring(0, 9))
+        System.out.printf("%s\n%s\n%s\n", text, text, text.substring(0, 9));
     }
 }
