248b0d75d954bb30b2fd29ba98661c5c34d271a1
Maniok19 <mano19029@hotmail.fr>
2025-05-22 12:54:50 +0200

    Fix formatting issues in PrintFormattingNumbers, PrintStringConcat, and PrintStringVariable classes

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -2,7 +2,7 @@ public class PrintStringConcat {
     public static void main(String[] args) {
         String text1 = "Holberton";
         String text2 = "School";
-        String text3 = text1 + text2;
-        System.out.printf("Welcome to %s !\n", text3);
+        String text3 = text1 + " " + text2;
+        System.out.printf("Welcome to %s!\n", text3);
     }
 }
--- 0x00-java-hello_world/PrintStringVariable.java
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -2,6 +2,6 @@ public class PrintStringVariable {
     public static void main(String[] args) {
         String text = "Holberton School";
         String holberton = text.substring(0, 9);
-        System.out.printf("%1$s\n%1$s\n%1$s\n%2$s\n", text, text.substring(0, 9));
+        System.out.printf("%s\n%s\n%s", text, text, holberton)
     }
 }
